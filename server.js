461f6f51b85ba9e07fc86f3abcd411aeefe23718
Lynn Miro <lynnmiro65@gmail.com>
2018-11-15 14:39:28 -0600

    adding logout & redux

--- server.js
+++ server.js
@@ -20,11 +20,14 @@ const db = require("./config/keys").mongoURI;
 // connect to MongoDB
 
 mongoose
-  .connect(db)
+  .connect(
+  db,
+  { useNewUrlParser: true } //need this for api support
+  )
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("hello world"));
+//app.get("/", (req, res) => res.send("hello world"));
 
 //passport middleware
 app.use(passport.initialize());
@@ -38,4 +41,5 @@ app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
+
 app.listen(port, () => console.log(`Server running on port ${port}`));
